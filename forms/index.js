f836b077d80d776eb1bb86c862f51a55d387d4aa
kunxin-chor <chorkunxin@yahoo.com>
2024-04-16 04:25:07 +0000

    Added CRUD

---
+++ forms/index.js
@@ -0,0 +1,51 @@
+// import in caolan forms
+const forms = require('forms');
+
+// create some shortcuts
+const fields = forms.fields;
+const validators = forms.validators;
+
+const bootstrapField = function (name, object) {
+    if (!Array.isArray(object.widget.classes)) { object.widget.classes = []; }
+
+    if (object.widget.classes.indexOf('form-control') === -1) {
+        object.widget.classes.push('form-control');
+    }
+
+    var validationclass = object.value && !object.error ? 'is-valid' : '';
+    validationclass = object.error ? 'is-invalid' : validationclass;
+    if (validationclass) {
+        object.widget.classes.push(validationclass);
+    }
+
+    var label = object.labelHTML(name);
+    var error = object.error ? '<div class="invalid-feedback">' + object.error + '</div>' : '';
+
+    var widget = object.widget.toHTML(name, object);
+    return '<div class="form-group">' + label + widget + error + '</div>';
+};
+
+
+// create a function that will
+// create a form object
+const createProductForm = () => {
+    // the object in the parameter
+    // is the form definition
+    return forms.create({
+        'name': fields.string({
+            required: true,
+            errorAfterField: true,
+        }),
+        'cost': fields.number({
+            required: true,
+            errorAfterField: true,
+            validators: [validators.min(0), validators.integer()]
+        }),
+        'description': fields.string({
+            required: true,
+            errorAfterField: true
+        })
+    })
+}
+
+module.exports = { createProductForm , bootstrapField};
--- routes/products.js
+++ routes/products.js
@@ -3,6 +3,7 @@ const router = express.Router();
 
 // require in the model
 const { Product } = require('../models');
+const { createProductForm, bootstrapField } = require('../forms');
 
 router.get('/', async function(req,res){
     // use the Product model to get all the products
@@ -14,8 +15,136 @@ router.get('/', async function(req,res){
 });
 
 router.get('/add-product', function(req,res){
-    res.send('Add product')
+    const productForm = createProductForm();
+    res.render('products/create', {
+        form: productForm.toHTML(bootstrapField)
+    })
 });
+
+router.post('/add-product', function(req,res){
+    // create the product form object using caolan form
+    const productForm = createProductForm();
+    // using the form object to handle the request
+    productForm.handle(req, {
+        'success': async function(form) {
+            // the forms has no error
+            // to access each field in the submitted form
+            // we use form.data.<fieldname>
+
+
+            // create an instance of the Product model
+            // an instance of a product is one row in the corresponding table
+            const product = new Product();
+            product.set('name', form.data.name)
+            product.set('cost', form.data.cost);
+            product.set('description', form.data.description);
+            // save the product to the database
+            await product.save();
+
+            // same as:
+            // INSERT INTO products (name, cost, description)
+            // VALUES (${form.data.name}, ${form.data.cost}, ${form.data.description})
+            res.redirect("/products/");
+        },
+        'empty': function(form) {
+            // the user submitted an empty form
+            res.render('products/create', {
+                form: productForm.toHTML(bootstrapField)
+            })
+        },
+        'error': function(form) {
+            // the user submitted a form with error
+            res.render('products/create', {
+                form: form.toHTML(bootstrapField)
+            })
+        }
+    })
+});
+
+router.get('/update-product/:productId', async function(req,res){
+    const productId = req.params.productId;
+    
+    // fetch the product that we want to update
+    // emulate: SELECT * from products WHERE id = ${productId}
+    const product = await Product.where({
+        'id': productId
+    }).fetch({
+        require: true
+    });
+
+    // create the product form
+    const productForm = createProductForm();
+
+    // prefill the form with values from the product 
+    productForm.fields.name.value = product.get('name');
+    productForm.fields.cost.value = product.get('cost');
+    productForm.fields.description.value = product.get('description');
+
+    res.render('products/update', {
+        'form': productForm.toHTML(bootstrapField),
+        'product': product.toJSON()
+    })
+});
+
+router.post('/update-product/:productId', async function(req,res){
+    // 1. create the form object
+    const productForm = createProductForm();
+
+    // 2. use the form object to handle the request
+    productForm.handle(req, {
+        'success':async function(form) {
+            // find the product that the user want to modify
+            const product = await Product.where({
+                'id': req.params.productId
+            }).fetch({
+                require: true  // make sure the product actually exists
+            });
+
+            // if every key in form.data is one column in a product row,
+            // we can use the following shortcut:
+            product.set(form.data);
+            await product.save();
+            res.redirect('/products/')
+        },
+        'empty': function(form) {
+            res.render('products/update', {
+                form: form.toHTML(bootstrapField)
+            })
+        },
+        'error': function(form) {
+            res.render('products/update', {
+                form: form.toHTML(bootstrapField)
+            })
+        }
+    })
+})
+
+router.get('/delete-product/:productId', async function(req,res){
+    const product = await Product.where({
+        'id': req.params.productId
+    }).fetch({
+        required: true
+    });
+
+    res.render('products/delete', {
+        product: product.toJSON()
+    })
+
+})
+
+router.post('/delete-product/:productId', async function(req,res){
+    // get the product which we want to delete
+    const product = await Product.where({
+        'id': req.params.productId
+    }).fetch({
+        required: true
+    });
+
+    await product.destroy();
+    res.redirect('/products');
+
+
+})
 
 // export
 module.exports = router;
